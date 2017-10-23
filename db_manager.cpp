a8602105b8dfa0c331d192d019525625150fe50f
José Cisneros <josecisneros654@gmail.com>
2017-10-23 12:49:40 -0300

    Update db_manager.cpp

--- db_manager.cpp
+++ db_manager.cpp
@@ -2,37 +2,6 @@
 
 DB_Manager::DB_Manager()
 {
-//SQLITE
-
-//    m_db = QSqlDatabase::addDatabase("QSQLITE");
-//    m_db.setDatabaseName("acopio.db");
-
-
-
-
-//AZURE
-
-//    QString conexion = "Driver={ODBC Driver 13 for SQL Server};"
-//                       "Server=tcp:dds2017.database.windows.net,1433;"
-//                       "Database=cdp_db2;"
-//                       "Uid=jcisneros@dds2017;"
-//                       "Pwd=CDP2017!;"
-//                       "Encrypt=yes;"
-//                       "TrustServerCertificate=no;"
-//                       "Connection Timeout=30;";
-//    m_db = QSqlDatabase::addDatabase("QODBC");
-//    m_db.setDatabaseName(conexion);
-
-
-
-//MARIADB
-
-    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
-    db.setHostName("emtarg.com.ar");
-    db.setUserName("emtargc1_jose12");
-    db.setPassword("a123456789");
-    db.setDatabaseName("emtargc1_diseno");
-    db.open();
 
 
 
